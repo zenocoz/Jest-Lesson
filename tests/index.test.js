f339a6f9ffd161d709ef6d03491a2679c996015e
Luis Antonio Canettoli <hello@luisanton.io>
2021-02-24 00:08:35 +0100

    Update index.test.js

--- tests/index.test.js
+++ tests/index.test.js
@@ -25,70 +25,70 @@ describe('I: Testing a test', () => {
     })
 })
 
-describe('II: Testing user creation and login', () => {
-    it("should test that the test route is returning 200", async () => {
-        const response = await request.get("/test")
-        const { message } = response.body
-        expect(message).toEqual("Test is success")
-    })
+// describe('II: Testing user creation and login', () => {
+//     it("should test that the test route is returning 200", async () => {
+//         const response = await request.get("/test")
+//         const { message } = response.body
+//         expect(message).toEqual("Test is success")
+//     })
 
-    it("should test that we can correctly create a user with the provided data", async () => {
-        const body = {
-            username: "luisanton.io",
-            password: "password"
-        }
-        const response = await request.post("/").send(body)
-        const { _id } = response.body
+//     it("should test that we can correctly create a user with the provided data", async () => {
+//         const body = {
+//             username: "luisanton.io",
+//             password: "password"
+//         }
+//         const response = await request.post("/").send(body)
+//         const { _id } = response.body
 
-        expect(_id).not.toBeNull()
+//         expect(_id).not.toBeNull()
 
-        const newUser = await UserModel.findById(_id)
-        expect(newUser).not.toBeNull()
-        expect(newUser.username).toEqual(body.username)
-    })
+//         const newUser = await UserModel.findById(_id)
+//         expect(newUser).not.toBeNull()
+//         expect(newUser.username).toEqual(body.username)
+//     })
 
-    // login with correct credentials
-    // -> has to receive a JWT token.
+//     // login with correct credentials
+//     // -> has to receive a JWT token.
 
-    it("should test that the login service is working", async () => {
-        const body = {
-            username: "luisanton.io",
-            password: "password"
-        }
+//     it("should test that the login service is working", async () => {
+//         const body = {
+//             username: "luisanton.io",
+//             password: "password"
+//         }
 
-        const response = await request.post("/login").send(body)
-        const { token } = response.body
+//         const response = await request.post("/login").send(body)
+//         const { token } = response.body
 
-        expect(token).toBe("FAKE_TOKEN")
-    })
-    // login with incorrect credentials
-    // -> has to receive a 400 response
-    it("should test that the login service is refusing login with wrong credentials", async () => {
-        const body = {
-            username: "luisanton.io",
-            password: "password2"
-        }
+//         expect(token).toBe("FAKE_TOKEN")
+//     })
+//     // login with incorrect credentials
+//     // -> has to receive a 400 response
+//     it("should test that the login service is refusing login with wrong credentials", async () => {
+//         const body = {
+//             username: "luisanton.io",
+//             password: "password2"
+//         }
 
-        const response = await request.post("/login").send(body)
-        const { token } = response.body
+//         const response = await request.post("/login").send(body)
+//         const { token } = response.body
 
-        expect(token).toBe(undefined)
-        expect(response.status).toBe(400)
-    })
+//         expect(token).toBe(undefined)
+//         expect(response.status).toBe(400)
+//     })
 
-    it("should test that the login service needs the correct data in input", async () => {
-        const body = {
-            username: "luisanton.io"
-        }
+//     it("should test that the login service needs the correct data in input", async () => {
+//         const body = {
+//             username: "luisanton.io"
+//         }
 
-        const response = await request.post("/login").send(body)
-        const { token } = response.body
+//         const response = await request.post("/login").send(body)
+//         const { token } = response.body
 
-        expect(token).toBe(undefined)
-        expect(response.status).toBe(400)
-    })
+//         expect(token).toBe(undefined)
+//         expect(response.status).toBe(400)
+//     })
 
 
 
-})
+// })
 
