b6e36e52e1b9d8fc0fc0f63a90b935a527453058
zenocoz <federico.soncini@gmail.com>
2021-02-24 16:19:50 +0100

    minor

--- src/services/users/index.js
+++ src/services/users/index.js
@@ -3,41 +3,38 @@ const UserSchema = require("./schema")
 const UserModel = require("mongoose").model("User", UserSchema)
 
 router.post("/register", async (req, res) => {
-    try {
-        const { username, password } = req.body
-        if (!username || !password) throw new Error("Provide credentials")
+  try {
+    const { username, password } = req.body
+    if (!username || !password) throw new Error("Provide credentials")
 
-        const user = new UserModel({ username, password })
-        const { _id } = await user.save()
+    const user = new UserModel({ username, password })
+    const { _id } = await user.save()
 
-        res.status(201).send({ _id })
-
-    } catch (error) {
-        res.status(400).send({
-            message: error.message,
-            errorCode: 'wrong_credentials'
-        })
-    }
+    res.status(201).send({ _id })
+  } catch (error) {
+    res.status(400).send({
+      message: error.message,
+      errorCode: "wrong_credentials",
+    })
+  }
 })
 
 router.post("/login", async (req, res) => {
-    try {
-        const { username, password } = req.body
-        if (!username || !password) throw new Error("Provide credentials")
+  try {
+    const { username, password } = req.body
+    if (!username || !password) throw new Error("Provide credentials")
 
-        const user = await UserModel.findOne({ username })
+    const user = await UserModel.findOne({ username })
 
-        user.password === password
-            ? res.status(200).send({ token: "VALID_TOKEN" })
-            : res.status(400).send({ message: "No username/password match" })
-
-    } catch (error) {
-        res.status(400).send({
-            message: error.message,
-            errorCode: 'wrong_credentials'
-        })
-    }
+    user.password === password
+      ? res.status(200).send({ token: "VALID_TOKEN" })
+      : res.status(400).send({ message: "No username/password match" })
+  } catch (error) {
+    res.status(400).send({
+      message: error.message,
+      errorCode: "wrong_credentials ",
+    })
+  }
 })
-
 
 module.exports = router
