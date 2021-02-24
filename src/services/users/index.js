93a1d5f947e043a47a49575e4953cfdc57a62d27
Luis Antonio Canettoli <hello@luisanton.io>
2021-02-24 02:39:53 +0100

    rollback

--- src/services/users/index.js
+++ src/services/users/index.js
@@ -2,11 +2,4 @@ const router = require("express").Router()
 const UserSchema = require("./schema")
 const UserModel = require("mongoose").model("User", UserSchema)
 
-router.post("/", async (req, res) => {
-    const user = new UserModel(req.body)
-    const { _id } = await user.save()
-
-    res.status(201).send({ _id })
-})
-
 module.exports = router
