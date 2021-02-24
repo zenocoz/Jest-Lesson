2f841dacdd56a5568f14b2430cfc39e7042f51e0
Luis Antonio Canettoli <hello@luisanton.io>
2021-02-24 10:26:09 +0100

    User Registration
    Tests + Implementation

--- src/services/users/index.js
+++ src/services/users/index.js
@@ -2,4 +2,19 @@ const router = require("express").Router()
 const UserSchema = require("./schema")
 const UserModel = require("mongoose").model("User", UserSchema)
 
+router.post("/register", async (req, res) => {
+    try {
+        const { username, password } = req.body
+        if (!username || !password) throw new Error("Provide credentials")
+
+        const user = new UserModel({ username, password })
+        const { _id } = await user.save()
+
+        res.status(201).send({ _id })
+
+    } catch (error) {
+        res.status(500).send({ message: error.message })
+    }
+})
+
 module.exports = router
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,27 +1,64 @@
-// const server = require("../src/server")
-// const request = require("supertest")(server)
+const server = require("../src/server")
+const request = require("supertest")(server)
+const mongoose = require("mongoose")
 
-// beforeAll((done) => {
-//     mongoose.connect(`${process.env.ATLAS_URL}/test`,
-//         { useNewUrlParser: true, useUnifiedTopology: true },
-//         () => {
-//             console.log("Successfully connected to Atlas.")
-//             done()
-//         });
-// });
+const UserSchema = require("../src/services/users/schema")
+const UserModel = require("mongoose").model("User", UserSchema)
 
-// afterAll((done) => {
-//     mongoose.connection.db.dropDatabase(() => {
-//         mongoose.connection.close(() => done())
-//     });
-// });
+beforeAll((done) => {
+    mongoose.connect(`${process.env.ATLAS_URL}/test`,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        () => {
+            console.log("Successfully connected to Atlas.")
+            done()
+        });
+});
+
+afterAll((done) => {
+    mongoose.connection.db.dropDatabase(() => {
+        mongoose.connection.close(() => done())
+    })
+})
+
 
 // I: Testing a test
 
-it("should check that true is true", () => { expect(true).toBe(true) })
+describe("Stage I: Testing tests", () => {
+    it("should check that true is true", () => {
+        expect(true).toBe(true)
+    })
+
+    it("should check that the /test endpoint is working correctly", async () => {
+        const response = await request.get("/test")
+        expect(response.status).toEqual(200)
+        expect(response.body.message).not.toBeFalsy()
+        expect(response.body.message).toEqual("Test success")
+    })
+
+})
 
 // II: Testing user creation and login
 
+describe("Stage II: testing user creation and login", () => {
+    it("should return an id from a /users/register endpoint when provided with valid credentials", async () => {
+        const validCredentials = {
+            username: "luisanton.io",
+            password: "password"
+        }
+
+        const response = await request.post("/users/register").send(validCredentials)
+
+        const { _id } = response.body
+        expect(_id).not.toBeFalsy()
+        expect(typeof _id).toBe("string")
+
+
+        const user = await UserModel.findById(_id)
+
+        expect(user).toBeDefined()
+
+    })
+})
 
 // III: Testing protected endpoints
 
